57e80e779e3205fa5edc22dc355118f94c161af9
bruno111A <141245558+bruno111A@users.noreply.github.com>
2024-10-30 12:21:44 -0300

    Add files via upload

---
+++ flashcard-aula_10/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Cinema',
+    'O que é um macguffin?',
+    'O é um termo utilizado para descrever algo que movimenta a trama, algo que tanto o protagonista quanto o antagonista buscam.'
+)
+
+criaCartao(
+    'Artes',
+    'Onde arranjavam tinta antigamente?',
+    'A partir de elementos da natureza, como minerais planta, até mesmo sangue e ovo foram utilizados para que os artistas conseguissem pintar.'
+)
+
+criaCartao(
+    'cinema',
+    'O que é um antogonista, ele é o vilão?',
+    'Um antagonista é aqule que vai contra o protagonista, alguém que tenta atrapalhar seus planos, mas vilão é um antagonista nsó que malvado.'
+)
+
+criaCartao(
+    'animação',
+    'O que stop motion?',
+    'É o processo de animação que se dá pela utilização de fotos de quadro por quadro das cenas, por isso o nome "stop motion" um movimento parado.'
+)
